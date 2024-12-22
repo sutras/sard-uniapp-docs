47531c8cc9e6ff28c6a4a8bce8e06c30c0a04304
wuzhitao <joinpass@outlook.com>
2024-12-22 22:40:07 +0800

    Updates

--- mobile/assets/usePageBackTop.D7SphWX6.js
+++
@@ -1,1 +0,0 @@
-import{f as o,J as r,K as s}from"./index-Vzcf13MH.js";function l(l=300){const n=o(0);r((o=>{n.value=o.scrollTop}));return{scrollTop:n,onClick:()=>{s({scrollTop:0,duration:l})}}}export{l as u};
