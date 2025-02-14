7f49ed04e6d982ddd60018c9c3e4fa5883bc4443
wuzhitao <joinpass@outlook.com>
2025-02-14 23:05:24 +0800

    Updates

---
+++ assets/index-DC-Biu-A.js
@@ -0,0 +1,2 @@
+import{M as t}from"./index-BOzO6DkT.js";import{d as o,c as r,o as n}from"./index-CQNqbkGh.js";const a=`<h1 id="首页">首页<a class="doc-anchor" href="#首页">#</a></h1>
+`,p=o({__name:"index",setup(c){const e={title:"首页",nav:{title:"首页",order:0,hidden:!0},redirect:"/guide/intro"};return(s,d)=>(n(),r(t,{content:a,fm:e}))}});export{p as default};
