59a560d98dd6a8f1c0b09a61e03bfc4d121e07d3
wuzhitao <joinpass@outlook.com>
2025-03-25 13:51:04 +0800

    Updates

---
+++ mobile/assets/usePageBackTop.08T7zXlk.js
@@ -0,0 +1,1 @@
+import{f as o,K as r,L as s}from"./index-Dz9mBhkx.js";function l(l=300){const n=o(0);r((o=>{n.value=o.scrollTop}));return{scrollTop:n,onClick:()=>{s({scrollTop:0,duration:l})}}}export{l as u};
