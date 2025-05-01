f6c9bc32f64a0f98ab52a5a73d757e49202410cc
wuzhitao <joinpass@outlook.com>
2025-05-01 21:31:45 +0800

    Updates

---
+++ assets/README-CO_QfIR4.js
@@ -0,0 +1,186 @@
+import{M as n}from"./index-CGrAObOf.js";import{d as s,c as t,o as p}from"./index-D4hYIIzu.js";const o=`<h2 id="介绍">介绍<a class="doc-anchor" href="#介绍">#</a></h2>
+<p>浮动在页面底部的面板，可以上下拖动来浏览内容，从而在不离开当前视图的情况下访问更多信息，常用于地图导航。</p>
+<h2 id="引入">引入<a class="doc-anchor" href="#引入">#</a></h2>
+<div class="doc-code-wrapper"><pre class="language-ts"><code class="language-ts"><span class="token keyword">import</span> FloatingPanel <span class="token keyword">from</span> <span class="token string">'sard-uniapp/components/floating-panel/floating-panel.vue'</span>
+</code></pre>
+</div><h2 id="代码演示">代码演示<a class="doc-anchor" href="#代码演示">#</a></h2>
+<h3 id="基础使用">基础使用<a class="doc-anchor" href="#基础使用">#</a></h3>
+<p><code>FloatingPanel</code> 的默认高度为 <code>100px</code>，用户可以拖动来展开面板，使高度达到 60% 的可用窗口高度。</p>
+<div class="doc-code-wrapper"><pre class="language-html"><code class="language-html"><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>template</span><span class="token punctuation">></span></span>
+  <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>doc-page</span> <span class="token attr-name">title</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">"</span>基础用法<span class="token punctuation">"</span></span><span class="token punctuation">></span></span>
+    <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>sar-floating-panel</span><span class="token punctuation">></span></span>
+      <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>sar-list</span><span class="token punctuation">></span></span>
+        <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>sar-list-item</span>
+          <span class="token attr-name">v-for</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">"</span>i in 26<span class="token punctuation">"</span></span>
+          <span class="token attr-name">:key</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">"</span>i<span class="token punctuation">"</span></span>
+          <span class="token attr-name">:title</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">"</span>String.fromCharCode(i + 64)<span class="token punctuation">"</span></span>
+        <span class="token punctuation">></span></span><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>sar-list-item</span><span class="token punctuation">></span></span>
+      <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>sar-list</span><span class="token punctuation">></span></span>
+    <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>sar-floating-panel</span><span class="token punctuation">></span></span>
+  <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>doc-page</span><span class="token punctuation">></span></span>
+<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>template</span><span class="token punctuation">></span></span>
+</code></pre>
+</div><h3 id="自定义锚点">自定义锚点<a class="doc-anchor" href="#自定义锚点">#</a></h3>
+<p>你可以通过 <code>anchors</code> 属性来设置 <code>FloatingPanel</code> 的锚点位置，并通过 <code>v-model:height</code> 来控制当前面板的显示高度。</p>
+<p>比如，使面板的高度在 100px、40% 可用窗口高度和 70% 可用窗口高度三个位置停靠：</p>
+<div class="doc-code-wrapper"><pre class="language-html"><code class="language-html"><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>template</span><span class="token punctuation">></span></span>
+  <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>doc-page</span> <span class="token attr-name">title</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">"</span>自定义锚点<span class="token punctuation">"</span></span><span class="token punctuation">></span></span>
+    <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>sar-floating-panel</span> <span class="token attr-name"><span class="token namespace">v-model:</span>height</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">"</span>height<span class="token punctuation">"</span></span> <span class="token attr-name">:anchors</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">"</span>anchors<span class="token punctuation">"</span></span><span class="token punctuation">></span></span>
+      <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>view</span> <span class="token special-attr"><span class="token attr-name">style</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">"</span><span class="token value css language-css"><span class="token property">padding</span><span class="token punctuation">:</span> 20rpx<span class="token punctuation">;</span> <span class="token property">text-align</span><span class="token punctuation">:</span> center</span><span class="token punctuation">"</span></span></span><span class="token punctuation">></span></span>
+        &#123;&#123; height.toFixed(0) &#125;&#125;
+      <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>view</span><span class="token punctuation">></span></span>
+      <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>sar-list</span><span class="token punctuation">></span></span>
+        <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>sar-list-item</span>
+          <span class="token attr-name">v-for</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">"</span>i in 26<span class="token punctuation">"</span></span>
+          <span class="token attr-name">:key</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">"</span>i<span class="token punctuation">"</span></span>
+          <span class="token attr-name">:title</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">"</span>String.fromCharCode(i + 64)<span class="token punctuation">"</span></span>
+        <span class="token punctuation">></span></span><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>sar-list-item</span><span class="token punctuation">></span></span>
+      <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>sar-list</span><span class="token punctuation">></span></span>
+    <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>sar-floating-panel</span><span class="token punctuation">></span></span>
+  <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>doc-page</span><span class="token punctuation">></span></span>
+<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>template</span><span class="token punctuation">></span></span>
+
+<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>script</span> <span class="token attr-name">lang</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">"</span>ts<span class="token punctuation">"</span></span> <span class="token attr-name">setup</span><span class="token punctuation">></span></span><span class="token script"><span class="token language-javascript">
+<span class="token keyword">import</span> <span class="token punctuation">&#123;</span> getWindowInfo <span class="token punctuation">&#125;</span> <span class="token keyword">from</span> <span class="token string">'sard-uniapp'</span>
+<span class="token keyword">import</span> <span class="token punctuation">&#123;</span> ref <span class="token punctuation">&#125;</span> <span class="token keyword">from</span> <span class="token string">'vue'</span>
+
+<span class="token keyword">const</span> <span class="token punctuation">&#123;</span> windowHeight <span class="token punctuation">&#125;</span> <span class="token operator">=</span> <span class="token function">getWindowInfo</span><span class="token punctuation">(</span><span class="token punctuation">)</span>
+
+<span class="token keyword">const</span> anchors <span class="token operator">=</span> <span class="token punctuation">[</span>
+  <span class="token number">100</span><span class="token punctuation">,</span>
+  Math<span class="token punctuation">.</span><span class="token function">round</span><span class="token punctuation">(</span><span class="token number">0.4</span> <span class="token operator">*</span> windowHeight<span class="token punctuation">)</span><span class="token punctuation">,</span>
+  Math<span class="token punctuation">.</span><span class="token function">round</span><span class="token punctuation">(</span><span class="token number">0.7</span> <span class="token operator">*</span> windowHeight<span class="token punctuation">)</span><span class="token punctuation">,</span>
+<span class="token punctuation">]</span>
+
+<span class="token keyword">const</span> height <span class="token operator">=</span> <span class="token function">ref</span><span class="token punctuation">(</span>anchors<span class="token punctuation">[</span><span class="token number">0</span><span class="token punctuation">]</span><span class="token punctuation">)</span>
+</span></span><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>script</span><span class="token punctuation">></span></span>
+</code></pre>
+</div><h3 id="仅头部拖拽">仅头部拖拽<a class="doc-anchor" href="#仅头部拖拽">#</a></h3>
+<p>默认情况下，<code>FloatingPanel</code> 的头部区域和内容区域都可以被拖拽，你可以通过 <code>content-draggable</code> 属性来禁用内容区域的拖拽。</p>
+<div class="doc-alert doc-alert-warning">
+  <div class="doc-alert-title">
+    <i class="doc-alert-icon hsi hsi-exclamation-circle"></i>
+    注意
+  </div>
+  <div class="doc-alert-content">
+<p>支付宝端内容区域始终不能拖拽，无论有没有设置 <code>content-draggable</code> 属性。</p>
+  </div>
+</div>
+<div class="doc-code-wrapper"><pre class="language-html"><code class="language-html"><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>template</span><span class="token punctuation">></span></span>
+  <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>doc-page</span> <span class="token attr-name">title</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">"</span>仅头部拖拽<span class="token punctuation">"</span></span><span class="token punctuation">></span></span>
+    <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>sar-floating-panel</span> <span class="token attr-name">:content-draggable</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">"</span>false<span class="token punctuation">"</span></span><span class="token punctuation">></span></span>
+      <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>view</span> <span class="token special-attr"><span class="token attr-name">style</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">"</span><span class="token value css language-css"><span class="token property">padding</span><span class="token punctuation">:</span> 20rpx<span class="token punctuation">;</span> <span class="token property">text-align</span><span class="token punctuation">:</span> center</span><span class="token punctuation">"</span></span></span><span class="token punctuation">></span></span>内容不可拖拽<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>view</span><span class="token punctuation">></span></span>
+      <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>sar-list</span><span class="token punctuation">></span></span>
+        <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>sar-list-item</span>
+          <span class="token attr-name">v-for</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">"</span>i in 26<span class="token punctuation">"</span></span>
+          <span class="token attr-name">:key</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">"</span>i<span class="token punctuation">"</span></span>
+          <span class="token attr-name">:title</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">"</span>String.fromCharCode(i + 64)<span class="token punctuation">"</span></span>
+        <span class="token punctuation">></span></span><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>sar-list-item</span><span class="token punctuation">></span></span>
+      <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>sar-list</span><span class="token punctuation">></span></span>
+    <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>sar-floating-panel</span><span class="token punctuation">></span></span>
+  <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>doc-page</span><span class="token punctuation">></span></span>
+<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>template</span><span class="token punctuation">></span></span>
+</code></pre>
+</div><h2 id="API">API<a class="doc-anchor" href="#API">#</a></h2>
+<h3 id="FloatingPanelProps">FloatingPanelProps<a class="doc-anchor" href="#FloatingPanelProps">#</a></h3>
+<div class="doc-table-responsive"><table><thead>
+<tr>
+<th>属性</th>
+<th>描述</th>
+<th>类型</th>
+<th>默认值</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>root-class</td>
+<td>组件根元素类名</td>
+<td>string</td>
+<td>-</td>
+</tr>
+<tr>
+<td>root-style</td>
+<td>组件根元素样式</td>
+<td>StyleValue</td>
+<td>-</td>
+</tr>
+<tr>
+<td>height</td>
+<td>当前面板的显示高度</td>
+<td>number</td>
+<td>0</td>
+</tr>
+<tr>
+<td>anchors</td>
+<td>设置自定义锚点, 单位 px</td>
+<td>number[]</td>
+<td>[100, uni.getWindowInfo().windowHeight * 0.6]</td>
+</tr>
+<tr>
+<td>duration</td>
+<td>动画时长，单位毫秒</td>
+<td>number</td>
+<td>300</td>
+</tr>
+<tr>
+<td>content-draggable</td>
+<td>允许拖拽内容容器</td>
+<td>boolean</td>
+<td>true</td>
+</tr>
+<tr>
+<td>safe-area-inset-bottom</td>
+<td>是否开启底部安全区适配</td>
+<td>boolean</td>
+<td>true</td>
+</tr>
+</tbody>
+</table></div><h3 id="FloatingPanelSlots">FloatingPanelSlots<a class="doc-anchor" href="#FloatingPanelSlots">#</a></h3>
+<div class="doc-table-responsive"><table><thead>
+<tr>
+<th>插槽</th>
+<th>描述</th>
+<th>属性</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>default</td>
+<td>自定义默认内容</td>
+<td>-</td>
+</tr>
+</tbody>
+</table></div><h3 id="FloatingPanelEmits">FloatingPanelEmits<a class="doc-anchor" href="#FloatingPanelEmits">#</a></h3>
+<div class="doc-table-responsive"><table><thead>
+<tr>
+<th>事件</th>
+<th>描述</th>
+<th>类型</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>update:height</td>
+<td>面板显示高度改变时触发</td>
+<td>&#123;value: number&#125;</td>
+</tr>
+<tr>
+<td>height-change</td>
+<td>面板显示高度改变且结束拖动后触发</td>
+<td>&#123;value: number&#125;</td>
+</tr>
+</tbody>
+</table></div><h2 id="主题定制">主题定制<a class="doc-anchor" href="#主题定制">#</a></h2>
+<h3 id="CSS变量">CSS 变量<a class="doc-anchor" href="#CSS变量">#</a></h3>
+<div class="doc-code-wrapper"><pre class="language-scss"><code class="language-scss"><span class="token selector">page </span><span class="token punctuation">&#123;</span>
+  <span class="token property">--sar-floating-panel-z-index</span><span class="token punctuation">:</span> 1000<span class="token punctuation">;</span>
+  <span class="token property">--sar-floating-panel-bg</span><span class="token punctuation">:</span> <span class="token function">var</span><span class="token punctuation">(</span>--sar-emphasis-bg<span class="token punctuation">)</span><span class="token punctuation">;</span>
+  <span class="token property">--sar-floating-panel-border-radius</span><span class="token punctuation">:</span> <span class="token function">var</span><span class="token punctuation">(</span>--sar-rounded-xl<span class="token punctuation">)</span><span class="token punctuation">;</span>
+
+  <span class="token property">--sar-floating-panel-header-height</span><span class="token punctuation">:</span> 64rpx<span class="token punctuation">;</span>
+  <span class="token property">--sar-floating-panel-bar-width</span><span class="token punctuation">:</span> 40rpx<span class="token punctuation">;</span>
+  <span class="token property">--sar-floating-panel-bar-height</span><span class="token punctuation">:</span> 6rpx<span class="token punctuation">;</span>
+  <span class="token property">--sar-floating-panel-bar-color</span><span class="token punctuation">:</span> <span class="token function">var</span><span class="token punctuation">(</span>--sar-tertiary-bg<span class="token punctuation">)</span><span class="token punctuation">;</span>
+<span class="token punctuation">&#125;</span>
+</code></pre>
+</div>`,k=s({__name:"README",setup(e){const a={nav:"组件",title:"FloatingPanel",subtitle:"浮动面板",group:"反馈组件",version:"1.13+"};return(c,l)=>(p(),t(n,{content:o,fm:a}))}});export{k as default};
