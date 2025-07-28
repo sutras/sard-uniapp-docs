09ea4a41f6733cecfad5529c9f702848c76bae67
wuzhitao <joinpass@outlook.com>
2025-07-28 23:44:43 +0800

    Updates

---
+++ mobile/assets/useWindowResize.Cy5vhHLo.js
@@ -0,0 +1,1 @@
+import{ac as i,bh as n,V as o,bi as s}from"./index-8t6gZryZ.js";function a(a){const t=i=>{a({windowWidth:i.size.windowWidth,windowHeight:i.size.windowHeight})};i((()=>{var i;null==(i=n)||i(t)})),o((()=>{var i;null==(i=s)||i(t)}))}export{a as u};
