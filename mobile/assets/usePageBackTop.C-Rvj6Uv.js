90e24f692f87a6e3269ae52193857eb8e55a39a3
wuzhitao <joinpass@outlook.com>
2025-09-06 17:15:24 +0800

    Updates

--- mobile/assets/usePageBackTop.C-Rvj6Uv.js
+++
@@ -1,1 +0,0 @@
-import{f as o,J as r,K as s}from"./index-CPR5Gd0r.js";function l(l=300){const n=o(0);r((o=>{n.value=o.scrollTop}));return{scrollTop:n,onClick:()=>{s({scrollTop:0,duration:l})}}}export{l as u};
