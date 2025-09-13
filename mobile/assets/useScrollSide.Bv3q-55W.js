a356211e6c95fa6e291b290dd9f4e3bc2c7d4421
wuzhitao <joinpass@outlook.com>
2025-09-14 01:27:07 +0800

    Updates

--- mobile/assets/useScrollSide.Bv3q-55W.js
+++
@@ -1,1 +0,0 @@
-import{f as l}from"./index-e_E3hn3v.js";function o(){const o=l(null);let e=0;return{scrollSide:o,onScroll:l=>{const r=l.detail.scrollTop;o.value=0===r?"start":e===r?"end":"center",e=r},onScrolltoupper:()=>{o.value="start"},onScrolltolower:()=>{o.value="end"}}}export{o as u};
