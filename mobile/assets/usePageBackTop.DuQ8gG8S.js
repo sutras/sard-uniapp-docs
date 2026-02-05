923e2a9e81fcaead2faebaedf824a9c50904dc88
wuzhitao <joinpass@outlook.com>
2026-02-05 14:15:37 +0800

    Updates

---
+++ mobile/assets/usePageBackTop.DuQ8gG8S.js
@@ -0,0 +1,1 @@
+import{f as o,N as r,O as s}from"./index-CIDJ3vZg.js";function l(l=300){const n=o(0);r((o=>{n.value=o.scrollTop}));return{scrollTop:n,onClick:()=>{s({scrollTop:0,duration:l})}}}export{l as u};
